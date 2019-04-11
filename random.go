9feb319f382b8dbcae041edf18db6cc6221c0899
skdrums <sora.kannaduki1009@gmail.com>
2019-04-11 15:11:36 +0900

    edit the file random.go

--- random.go
+++ random.go
@@ -1,53 +1,57 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
-	"encoding/csv"
 	"os"
 	"strconv"
 )
 
 func failOnError(err error) {
-    if err != nil {
-        log.Fatal("Error:", err)
-    }
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
 }
 
 type Tile struct {
-	id int
+	id   int
 	name string
 	kind string
+	imageName string
+	japaneseName string
 }
 
-func newTile(id int, name,kind string) *Tile{
+func newTile(id int, name, kind, imageName, japaneseName string) *Tile {
 	t := new(Tile)
 	t.id = id
 	t.name = name
 	t.kind = kind
+	t.imageName = imageName
+	t.japaneseName = japaneseName
 	return t
 }
 
 func main() {
-    // ファイルを開く
-    file, err := os.Open("./data/tile.csv")
-    failOnError(err)
+	// ファイルを開く
+	file, err := os.Open("./data/tile.csv")
+	failOnError(err)
 
-    // プログラムが終わったらファイルを閉じる
-    defer file.Close()
+	// プログラムが終わったらファイルを閉じる
+	defer file.Close()
 
-    // // 12byte格納可能なスライスを用意する
-    // message := make([]byte, 12)
+	// // 12byte格納可能なスライスを用意する
+	// message := make([]byte, 12)
 
-    // // ファイル内のデータをスライスに読み出す
-    // _, err = file.Read(message)
-    // if err != nil { // エラー処理
-    //     log.Fatal(err)
-    // }
+	// // ファイル内のデータをスライスに読み出す
+	// _, err = file.Read(message)
+	// if err != nil { // エラー処理
+	//     log.Fatal(err)
+	// }
 
-    // // 文字列にして表示
+	// // 文字列にして表示
 	// fmt.Print(string(message))
-	
+
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
 	failOnError(err)
@@ -57,11 +61,12 @@ func main() {
 		if key == 0 {
 			continue
 		}
-		id,_ := strconv.Atoi(record[0])
-		tiles = append(tiles,*newTile(id,record[1],record[2]))
+		id, _ := strconv.Atoi(record[0])
+		tiles = append(tiles, *newTile(id, record[1], record[2], record[3], record[4]))
 	}
 	for _, tile := range tiles {
 		fmt.Println(tile)
 		fmt.Printf("\n")
 	}
+	fmt.Println(t.japaneseName)
 }
