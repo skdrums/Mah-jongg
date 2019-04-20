6474f9cad0822af650beca808a38a70273333fb3
skdrums <sora.kannaduki1009@gmail.com>
2019-04-20 23:03:23 +0900

    create finishdata

--- Score/scoreCalculation.go
+++ Score/scoreCalculation.go
@@ -21,61 +21,79 @@ type Finisher struct {
 type FinishData struct {
 	EndTiles                 []Tile.Tile
 	Mentsu                   *[4]Mentsu
-	Finisher, Dora, HeadTile Tile.Tile
+	Finisher, Dora, HeadTile *Tile.Tile
 	PublicWind, PrivateWind  Tile.Name
 }
 
 //鳴きの考慮なし＠要編集
 func NewMentsu(tiles *[3]Tile.Tile) *Mentsu {
-	mentsu := &Mentsu{Tiles : tiles, IsCalled: true} 
+	mentsu := &Mentsu{Tiles: tiles, IsCalled: true}
 	return mentsu
 }
 
 //面前判定の考慮なし＠要編集
 func NewFinisher(tile *Tile.Tile) *Finisher {
-	finisher := &Finisher{Tile:tile,IsTsumo:true}
+	finisher := &Finisher{Tile: tile, IsTsumo: true}
 	return finisher
 }
 
+var k int = 0
+var u = Tile.Tile{}
+var flagOfPatterns int = 0
+
+//var patterns = [3]FinishData{*NewFinishData(),*NewFinishData(t),*NewFinishData(t)}
+
 func backtrack(i int, t []Tile.Tile, d *FinishData) {
-	u := Tile.Tile{}
-	k := 0
+	fmt.Printf("@i = %d \n", i)
+	fmt.Println(bool(d.HeadTile == &u))
+	fmt.Println(d.HeadTile)
+	if i == 14 {
+		for _, mentsu := range d.Mentsu {
+			for _, dataTile := range mentsu.Tiles {
+				fmt.Printf("%s|", dataTile.JapaneseName)
+			}
+		}
+		i := 0
+		for i < 2 {
+			fmt.Printf("%s|", d.HeadTile.JapaneseName)
+			i++
+		}
+	}
 	for j := i + 1; j < 14 && j-i < 3; j++ {
-		fmt.Printf("i = %d,j = %d  ", i, j)
-		if j == i+1 && t[j].ID == t[i].ID && d.HeadTile == u {
-			d.HeadTile = t[i]
+
+		fmt.Printf("@i = %d,j = %d  \n", i, j)
+		if j == i+1 && t[j].ID == t[i].ID && *d.HeadTile == u { //雀頭
+			d.HeadTile = &t[i]
+			fmt.Println(bool(*d.HeadTile == u))
+			fmt.Printf("ヘッドだよ")
 			fmt.Println(d.HeadTile)
 			backtrack(j+1, t, d)
-		} else if j == i+2 && t[j].Kind == t[i].Kind {
-			slice := t[i:3]
+			d.HeadTile = &u
+		} else if j == i+2 && t[j].Kind == t[i].Kind && t[j].ID == t[i].ID+Tile.ID(2) && t[j].ID == t[i+1].ID+Tile.ID(1) { // 順子
+			slice := t[i : i+3]
 			array := [3]Tile.Tile{}
-			for i,s :=range slice{
-				array[i]=s
+			for key, s := range slice {
+				array[key] = s
 			}
-			fmt.Printf("array = ")
-			fmt.Println(array)
-			shuntsu := NewMentsu(&array)
-			fmt.Printf("shuntsu = ")
-			fmt.Println(shuntsu.Tiles)
-			d.Mentsu[k] = *shuntsu
+			for key, tile := range array {
+				d.Mentsu[k].Tiles[key] = tile
+			}
+			// fmt.Printf("Mentsu[%d].Tiles = ", k)
+			// fmt.Println(d.Mentsu[k].Tiles)
 			k++
-			fmt.Println(d.Mentsu[k].IsCalled)
-			for _, tile := range d.Mentsu[k].Tiles {
-				fmt.Println(tile)
-			}
 			backtrack(j+1, t, d)
 			k--
-		} else if j == i+2 && t[j].ID == t[i].ID {
-			slice := t[i:3]
+		} else if j == i+2 && t[j].ID == t[i].ID { // 暗子
+			slice := t[i : i+3]
 			array := [3]Tile.Tile{}
-			for i,s :=range slice{
-				array[i]=s
+			for i, s := range slice {
+				array[i] = s
 			}
-			d.Mentsu[k] = *NewMentsu(&array)
-			fmt.Println(d.Mentsu[k].IsCalled)
-			for _, tile := range d.Mentsu[k].Tiles {
-				fmt.Println(tile)
+			for key, tile := range array {
+				d.Mentsu[k].Tiles[key] = tile
 			}
+			// fmt.Printf("Mentsu[%d].Tiles = ", k)
+			// fmt.Println(d.Mentsu[k].Tiles)
 			k++
 			backtrack(j+1, t, d)
 			k--
@@ -84,14 +102,20 @@ func backtrack(i int, t []Tile.Tile, d *FinishData) {
 }
 
 func NewFinishData(endTiles []Tile.Tile) *FinishData {
-	finishData := new(FinishData)
-	finishData.EndTiles = endTiles
+	tiles := [4][3]Tile.Tile{}
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 3; j++ {
+			tiles[i][j] = *new(Tile.Tile)
+		}
+	}
+	mentsu := [4]Mentsu{*NewMentsu(&tiles[0]), *NewMentsu(&tiles[1]), *NewMentsu(&tiles[2]), *NewMentsu(&tiles[3])}
+	finishData := FinishData{EndTiles: endTiles, Mentsu: &mentsu, Finisher: new(Tile.Tile), Dora: new(Tile.Tile), HeadTile: new(Tile.Tile)}
 
-	return finishData
+	return &finishData
 }
 
-func DevideTiles(endTiles []Tile.Tile) *FinishData{
-	finishData:=NewFinishData(endTiles)
+func DevideTiles(endTiles []Tile.Tile) *FinishData {
+	finishData := NewFinishData(endTiles)
 	backtrack(0, endTiles, finishData)
 
 	return finishData
